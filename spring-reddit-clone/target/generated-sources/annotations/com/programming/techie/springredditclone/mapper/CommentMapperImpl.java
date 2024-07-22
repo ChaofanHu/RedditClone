ada370b5f5da285cee9ce900c69425bb99f54f4c
ChaofanHu <chaofan.hu@usi.ch>
2024-07-22 13:45:58 +0200

    test

---
+++ spring-reddit-clone/target/generated-sources/annotations/com/programming/techie/springredditclone/mapper/CommentMapperImpl.java
@@ -0,0 +1,53 @@
+package com.programming.techie.springredditclone.mapper;
+
+import com.programming.techie.springredditclone.dto.CommentsDto;
+import com.programming.techie.springredditclone.model.Comment;
+import com.programming.techie.springredditclone.model.Post;
+import com.programming.techie.springredditclone.model.User;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-07-22T13:24:14+0200",
+    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.8 (Oracle Corporation)"
+)
+@Component
+public class CommentMapperImpl implements CommentMapper {
+
+    @Override
+    public Comment map(CommentsDto commentsDto, Post post, User user) {
+        if ( commentsDto == null && post == null && user == null ) {
+            return null;
+        }
+
+        Comment comment = new Comment();
+
+        if ( commentsDto != null ) {
+            comment.setText( commentsDto.getText() );
+        }
+        comment.setPost( post );
+        comment.setUser( user );
+        comment.setCreatedDate( java.time.Instant.now() );
+
+        return comment;
+    }
+
+    @Override
+    public CommentsDto mapToDto(Comment comment) {
+        if ( comment == null ) {
+            return null;
+        }
+
+        CommentsDto commentsDto = new CommentsDto();
+
+        commentsDto.setId( comment.getId() );
+        commentsDto.setCreatedDate( comment.getCreatedDate() );
+        commentsDto.setText( comment.getText() );
+
+        commentsDto.setPostId( comment.getPost().getPostId() );
+        commentsDto.setUserName( comment.getUser().getUsername() );
+
+        return commentsDto;
+    }
+}
